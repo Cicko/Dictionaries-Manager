c8f295d384951e620841bb5023a76d1527751e24
Cicko <rudolfcicko2016@gmail.com>
2019-02-13 17:57:38 +0100

    Editing the navigation drawer items

--- app/components/NavDrawer/navItems.js
+++ app/components/NavDrawer/navItems.js
@@ -1,3 +1,13 @@
-export default {
+import HomeIcon from '@material-ui/icons/Home';
+import BooksIcon from '@material-ui/icons/LibraryBooks'
 
-}
+export default [
+  {
+    Icon: HomeIcon,
+    label: 'Home',
+  },
+  {
+    Icon: BooksIcon,
+    label: 'Dictionaries',
+  },
+];
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -7,31 +7,51 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
+import { compose } from 'recompose';
 import { get } from 'lodash';
+import Grid from '@material-ui/core/Grid';
 import Table from '../../components/Table';
 import NavDrawer from '../../components/NavDrawer';
 import AppBar from '../../components/AppBar';
+
+const styles = {
+  tableContainer: {
+  },
+};
 
 /* eslint-disable react/prefer-stateless-function */
 class HomePage extends React.PureComponent {
   render() {
     return (
-      <div>
+      <Grid container>
         <AppBar />
         <NavDrawer open={this.props.drawerOpen} />
-        <Table />
-      </div>
+        <Grid
+          container
+          className={this.props.classes.tableContainer}
+          justify="center"
+        >
+          <Grid>
+            <Table />
+          </Grid>
+        </Grid>
+      </Grid>
     );
   }
 }
 
 HomePage.propTypes = {
   drawerOpen: PropTypes.bool,
+  classes: PropTypes.object.isRequired,
 }
 
-export default connect(
-  state => ({
-    drawerOpen: get(state, 'generic.navDrawer.open', false),
-  })
+export default compose(
+  connect(
+    state => ({
+      drawerOpen: get(state, 'generic.navDrawer.open', false),
+    })
+  ),
+  withStyles(styles),
 )(HomePage);
